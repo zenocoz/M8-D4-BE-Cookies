63ff077cb14a2a9c078b30c79e6003528a7eafe8
zenocoz <federico.soncini@gmail.com>
2021-02-16 14:25:07 +0100

    all files

--- server.js
+++ server.js
@@ -1,1 +1,9 @@
 const express = require("express")
+
+const server = express()
+
+const port = process.env.PORT || 3005
+
+server.listen(port, () => {
+  console.log("server listening on port " + port)
+})
